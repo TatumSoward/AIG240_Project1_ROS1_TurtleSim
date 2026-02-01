96668aef3b1714afbc20901df2c340b5bd4166f8
jetauto <jetauto@AIG240-ubuntu-18.myguest.virtualbox.org>
2026-02-01 12:53:31 -0500

    Commit of new workspace

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/jetauto/project01_ws/src
+# Build directory: /home/jetauto/project01_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("lab3_turtlesim")
---
+++ build/lab3_turtlesim/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jetauto/project01_ws/src/lab3_turtlesim
+# Build directory: /home/jetauto/project01_ws/build/lab3_turtlesim
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
